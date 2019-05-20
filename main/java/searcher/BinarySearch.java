70543eade6300a043418414fd3bf0d9877a1adae
Ankitagole <ankita17@navgurukul.org>
2019-05-20 22:55:02 +0530

    Refactor loop.

--- main/java/searcher/BinarySearch.java
+++ main/java/searcher/BinarySearch.java
@@ -3,26 +3,26 @@ package searcher;
 public class BinarySearch {
     public static void main(String[] args) {
         int toBeSearched = Integer.parseInt(args[0]);
-        int[] search = {1, 3, 4, 6, 7, 11, 14, 22};
-        int largeNumber = search.length;
-        int smalllNumber = 0;
-        findingNumber(toBeSearched, search, largeNumber, smalllNumber);
+        int[] searchIn = {1, 3, 4, 6, 7, 11, 14, 22};
+        findNumberInList(toBeSearched, searchIn);
     }
 
-    public static int findingNumber(int toBeSearch, int[] searching, int largeNumber, int smallNumber) {
-        for (int i = 0; i < searching.length; i++) {
-            int middleNumber = gettingMiddleNumber(largeNumber, smallNumber);
-            if (searching[middleNumber] == toBeSearch) {
-                return (middleNumber);
-            } else if (searching[middleNumber] > toBeSearch) {
+    public static int findNumberInList(int toBeSearch, int[] searchIn) {
+        int largeNumber = searchIn.length;
+        int smallNumber = 0;
+        while (smallNumber < largeNumber-1) {
+            int middleNumber = getMiddleNumber(largeNumber, smallNumber);
+            if (searchIn[middleNumber] == toBeSearch) {
+                return middleNumber;
+            } else if (searchIn[middleNumber] > toBeSearch) {
                 largeNumber = middleNumber;
-            } else if (searching[middleNumber] < toBeSearch) {
+            } else if (searchIn[middleNumber] < toBeSearch) {
                 smallNumber = middleNumber;
             }
         }
         return (-1);
     }
-    private static int gettingMiddleNumber(int largeNumber, int smallNumber) {
+    private static int getMiddleNumber(int largeNumber, int smallNumber) {
         return (largeNumber + smallNumber) / 2;
     }
 }
--- test/java/searcher/BinaryTest.java
+++ test/java/searcher/BinaryTest.java
@@ -2,23 +2,18 @@ package searcher;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
-
 import static junit.framework.TestCase.assertEquals;
 
 public class BinaryTest {
     @Test public void NumberDoesNotExist(){
-        int[] searching = {1,3, 4, 6, 7, 11, 14, 22};
-        int smalllNumber = 0;
+        int[] searchIn = {1,3, 4, 6, 7, 11, 14, 22};
         int expected = -1;
-        assertEquals(expected,BinarySearch.findingNumber(9,searching,7,smalllNumber));
+        assertEquals(expected,BinarySearch.findNumberInList(9,searchIn));
     }
     @Test public void NumberExist(){
-        int[] searching = {1,3, 4, 6, 7, 11, 14, 22};
-        int smalllNumber = 0;
+        int[] searchIn = {1,3, 4, 6, 7, 11, 14, 22};
         int expected = 2;
-        assertEquals(expected,BinarySearch.findingNumber(4,searching,7,smalllNumber));
+        assertEquals(expected,BinarySearch.findNumberInList(4,searchIn));
     }
 }
 
