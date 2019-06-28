452134bb578af040c0c57e7511164fde91bc3f6f
Ankitagole <ankita17@navgurukul.org>
2019-06-28 06:55:38 +0530

    initial commit

--- main/java/searcher/Searcher.java
+++ main/java/searcher/Searcher.java
@@ -8,25 +8,6 @@ public class Searcher implements Comparable{
 
     @Override
     public int compareTo(Object o) {
-
         return searchIn;
     }
 }
-
-
-//    public List<Runner> runnerList = new ArrayList<>();
-//
-//    public void addRunner(Runner runner){
-//        runnerList.add(runner);
-//    }
-//    public Runner getWinner(){
-//        Comparator<Runner> comp = new Comparator<>() {
-//            @Override
-//            public int compare(Runner o1, Runner o2) {
-//                return (int) (o1.finishTime - o2.finishTime);
-//            }
-//        };
-//        Runner runner = Collections.min(runnerList, comp);
-//        return runner;
-//    }
-//}
